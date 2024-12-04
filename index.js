f77f4e2820e61edfe8b18487b7d2af1a8aff2aa1
Juwel Rana <juwel.jem@gmail.com>
2024-12-04 09:07:02 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const {
+  MongoClient,
+  ServerApiVersion,
+  Collection,
+  ObjectId,
+} = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +17,23 @@ app.get("/", (req, res) => {
   res.send("Server running Fine!");
 });
 
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.PASSWORD}@cluster0.hjkzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Server is running on ", port);
